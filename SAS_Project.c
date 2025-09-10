4f69807de81d9bbf55d8466633403e17a97d9dd4
lahcen404 <lahcen.maskour2003@gmail.com>
2025-09-10 12:27:26 +0200

    create function of search by name

--- SAS_Project.c
+++ SAS_Project.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
+#include <string.h>
+
 #define MAX 100
 
-typedef struct {
+typedef struct
+{
     int id;
     char nom[50];
     char prenom[50];
@@ -15,72 +18,78 @@ Joueur joueurs[MAX];
 int countJoueurs = 0;
 
 
+
 // ajouter un joueur
-void ajouterUnJoueur(){
+void ajouterUnJoueur()
+{
 
     joueurs[countJoueurs].id = countJoueurs + 1;
 
     printf("Entrer le nom de joueur :\n");
-    fgets(joueurs[countJoueurs].nom,sizeof(joueurs[countJoueurs].nom),stdin);
+    fgets(joueurs[countJoueurs].nom, sizeof(joueurs[countJoueurs].nom), stdin);
+    joueurs[countJoueurs].nom[strcspn(joueurs[countJoueurs].nom, "\n")] = '\0'; // removee enter key from the end 
+
 
     printf("Entrer le prenom de joueur :\n");
-    fgets(joueurs[countJoueurs].prenom,sizeof(joueurs[countJoueurs].prenom),stdin);
+    fgets(joueurs[countJoueurs].prenom, sizeof(joueurs[countJoueurs].prenom), stdin);
+    joueurs[countJoueurs].prenom[strcspn(joueurs[countJoueurs].prenom, "\n")]= '\0';
 
     printf("Entrer le numero Maillot de joueur :\n");
-    scanf("%d",&joueurs[countJoueurs].numeroMaillot);
+    scanf("%d", &joueurs[countJoueurs].numeroMaillot);
     getchar();
 
     printf("Entrer le poste de joueur ([G] = Gardien, [D] = Défenseur, [M] = Millieu,[G] = Attaquant)) :\n");
-    fgets(joueurs[countJoueurs].poste,sizeof(joueurs[countJoueurs].poste),stdin);
+    fgets(joueurs[countJoueurs].poste, sizeof(joueurs[countJoueurs].poste), stdin);
+    joueurs[countJoueurs].poste[strcspn(joueurs[countJoueurs].poste, "\n")]= '\0';
+
 
     printf("Entrer l'age de joueur :\n");
-    scanf("%d",&joueurs[countJoueurs].age);
+    scanf("%d", &joueurs[countJoueurs].age);
     getchar();
 
     printf("Entrer le nombre de buts de joueur :\n");
-    scanf("%d",&joueurs[countJoueurs].buts);
+    scanf("%d", &joueurs[countJoueurs].buts);
     getchar();
 
-   
     countJoueurs++;
 }
 
-
 // ajouter plusieur joueurs
-void ajouterPlusieurJoueurs(){
+void ajouterPlusieurJoueurs()
+{
 
     int n;
 
     printf("Entrer nombre joueurs :");
-    scanf("%d",&n);
+    scanf("%d", &n);
     getchar();
 
     for (int i = 0; i < n; i++)
     {
-        printf("\n Joueur %d\n",i+1);
+        printf("\n Joueur %d\n", i + 1);
         ajouterUnJoueur();
     }
-    
-
 }
 
-
 // afficher joueurs
-void afficherJoueurs() {
-    if (countJoueurs == 0) {
+void afficherJoueurs()
+{
+    if (countJoueurs == 0)
+    {
         printf("\n Aucun joueur !!\n");
         return;
     }
 
     printf("\n========== Liste des Joueurs ==========\n");
 
-    for (int i = 0; i < countJoueurs; i++) {
+    for (int i = 0; i < countJoueurs; i++)
+    {
         printf("\n[Joueur %d]\n", i + 1);
         printf("ID              : %d\n", joueurs[i].id);
-        printf("Nom             : %s", joueurs[i].nom);
-        printf("Prenom          : %s", joueurs[i].prenom);
+        printf("Nom             : %s\n", joueurs[i].nom);
+        printf("Prenom          : %s\n", joueurs[i].prenom);
         printf("Numero Maillot  : %d\n", joueurs[i].numeroMaillot);
-        printf("Poste           : %s", joueurs[i].poste);
+        printf("Poste           : %s\n", joueurs[i].poste);
         printf("Age             : %d\n", joueurs[i].age);
         printf("Nombre de Buts  : %d\n", joueurs[i].buts);
         printf("---------------------------------------\n");
@@ -89,10 +98,48 @@ void afficherJoueurs() {
     printf("=======================================\n");
 }
 
-int main(){
+// recherche par Nom
+int rechercherJoueurNom(char nomRecherche[50])
+{
 
-      int choix;
-    do{
+    printf("Entrez le nom du joueur : ");
+    fgets(nomRecherche, sizeof(nomRecherche), stdin);
+    nomRecherche[strcspn(nomRecherche, "\n")]= '\0';
+
+
+
+    printf("%s", nomRecherche);
+
+    for (int i = 0; i < countJoueurs; i++)
+    {
+        if (strcasecmp(joueurs[i].nom, nomRecherche) == 0)
+        {
+            
+                    printf("\n[Joueur %d]\n", i + 1);
+                    printf("ID              : %d\n", joueurs[i].id);
+                    printf("Nom             : %s\n", joueurs[i].nom);
+                    printf("Prenom          : %s\n", joueurs[i].prenom);
+                    printf("Numero Maillot  : %d\n", joueurs[i].numeroMaillot);
+                    printf("Poste           : %s\n", joueurs[i].poste);
+                    printf("Age             : %d\n", joueurs[i].age);
+                    printf("Nombre de Buts  : %d\n", joueurs[i].buts);
+                    printf("---------------------------------------\n");
+                
+            
+
+            return i;
+        }
+    }
+    printf("Joueur non trouvé !\n");
+    return -1;
+}
+
+int main()
+{
+
+    int choix;
+    do
+    {
         printf("\n-----------------------------------\n");
         printf("\n--- Gestion Equipe de Football ---\n");
         printf("\n-----------------------------------\n");
@@ -105,15 +152,18 @@ int main(){
         printf("7 - Statistiques\n");
         printf("8 - Quitter\n");
         printf("Entrez votre choix : \n");
-        scanf("%d",&choix);
+        scanf("%d", &choix);
         getchar();
 
-         switch(choix){
-            case 1:
+        switch (choix)
+        {
+        case 1:
             int ajoutChoix;
             printf("\n1 - Ajouter un nouveau joueur\n");
             printf("2 - Ajouter plusieur joueurs\n");
-            scanf("%d",&ajoutChoix);
+            printf("3 - Quit\n");
+
+            scanf("%d", &ajoutChoix);
             getchar();
             switch (ajoutChoix)
             {
@@ -124,22 +174,39 @@ int main(){
             case 2:
                 ajouterPlusieurJoueurs();
                 break;
-            
-            default: printf("Choix invalide !\n");
+
+            case 3:
+                printf("Quit !\n");
+                break;
+
+            default:
+                printf("Choix invalide !\n");
                 break;
             }
             break;
 
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            afficherJoueurs();
+            break;
+        case 5:
+            break;
 
-            case 2:  break;
-            case 3:  break;
-            case 4: afficherJoueurs();  break;
-            case 5:  break;
-            case 6:  break;
-            case 7:  break;
-            case 8: printf("Au revoir !!\n"); break;
-            default: printf("Choix invalide !\n");
+        case 6:
+            char nomRecherche[50];
+            rechercherJoueurNom(nomRecherche);
+            break;
+
+        case 7:
+            break;
+        case 8:
+            printf("Au revoir !!\n");
+            break;
+        default:
+            printf("Choix invalide !\n");
         }
-    }while(choix!=8);
-    
+    } while (choix != 8);
 }
